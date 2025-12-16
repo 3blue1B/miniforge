7533936da0dd359eb3637b586d13fb6b37fc47dc
ldh <ldh18323561151@gmail.com>
2025-12-16 20:15:23 +0800

    second try

---
+++ week1/lea1.py
@@ -0,0 +1,8 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+tenser_2d = torch.rand(3,4)
+print(tenser_2d)
+tenser_3d = torch.rand(1,2,3)
+print(tenser_3d)
+print(tenser_2d.dtype)
